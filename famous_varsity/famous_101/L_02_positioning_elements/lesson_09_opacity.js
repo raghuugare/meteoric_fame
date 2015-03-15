2650e65f820ff0ef4c4ff32e2909bba858d4c160
Raghavendra Ugare <raghu.ugare@gmail.com>
2015-03-16 01:08:52 +0530

    Lesson_09: Opacity! :)

---
+++ famous_varsity/famous_101/L_02_positioning_elements/lesson_09_opacity.js
@@ -0,0 +1,31 @@
+/*
+
+   L09. Opacity
+
+   We can also set opacity in a modifier. Then everything that gets added to the render tree after that modifier gets that opacity value multiplied to its opacity.
+ */
+
+var Engine = require('famous/core/Engine');
+var Surface = require('famous/core/Surface');
+var Transform = require('famous/core/Transform');
+var StateModifier = require('famous/modifiers/StateModifier');
+
+var mainContext = Engine.createContext();
+
+var surface = new Surface({
+    size: [100, 100],
+    content: 'stretch',
+    properties: {
+        color: 'white',
+        backgroundColor: '#FA5C4F'
+    }
+});
+
+var modifier = new StateModifier({
+    opacity: 0.5,
+    transform: Transform.scale(1, 3, 1),
+    align: [0.5, 0.5],
+    origin: [0.5, 0.5]
+});
+
+mainContext.add(modifier).add(surface);
